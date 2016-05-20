59277829c92974898d223ea41f762945b39c5c18
Jed <jean-edouard@users.noreply.github.com>
2016-05-20 15:33:59 -0400

    Create cloner.sh

---
+++ cloner.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+manifest="$1"
+
+rm -rf git
+mkdir git
+
+while read line; do
+    repo=$(echo $line | sed 's/^Fetching //' | cut -d ' ' -f 1 | sed 's/\.git//')
+    gref=$(echo $line | sed 's/^Fetching //' | cut -d ' ' -f 2)
+    git clone https://github.com/OpenXT/$repo git/$repo
+    cd git/$repo
+    git branch magic $gref
+    cd -
+done < manifest
